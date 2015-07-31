5efed49dcdcdcb6857e68b5d15c3cb14ea335d34
unknown <pair-station@foobar.com>
2015-07-31 16:14:24 -0400

    Adding grunt...

---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+/*jshint node:true */
+
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      all: 'dist'
+    },
+
+    cssmin: {
+      all: {
+        files: {
+          'dist/main.css': 'src/*.css'
+        }
+      }
+    },
+
+    copy: {
+      all: {
+        files: [
+          {
+            cwd: 'src',
+            src: '*.{html,png,jpg}',
+            dest: 'dist/'
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('foo', function() {
+    console.log('I am in a foo!');
+  });
+
+  grunt.registerTask('bar', function() {
+    console.log('I am in a bar!');
+  });
+
+  grunt.registerTask('foobar', ['foo', 'bar']);
+
+  grunt.registerTask('build', [
+    'clean',
+    'cssmin'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
+};
